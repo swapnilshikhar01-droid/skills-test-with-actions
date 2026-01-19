7521aab6fa7ea174906243e9fdba5784be048a80
swapnilshikhar01-droid <swapnilshikhar01@gmail.com>
2026-01-19 22:18:52 +0530

    Update calculations_test.py

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -57,14 +57,3 @@ def test_get_nth_fibonacci_one():
     # Assert
     assert result == 1
 
-
-# def test_get_nth_fibonacci_ten():
-#     """Test with n=10."""
-#     # Arrange
-#     n = 10
-
-#     # Act
-#     result = get_nth_fibonacci(n)
-
-#     # Assert
-#     assert result == 89
